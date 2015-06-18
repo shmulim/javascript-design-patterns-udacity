8901d7c2e6ba7b8c518a84176fb0dc934142e67f
@shmulim <shmuel.markel@gmail.com>
2015-06-17 18:27:34 -0700

    Create index.js

---
+++ week-1/cat-clicker/first-iteration/index.js
@@ -0,0 +1,7 @@
+var cat = document.getElementById('cat');
+var counter = document.getElementById('counter');
+var count = 0;
+
+cat.addEventListener('click', function () {
+    counter.innerHTML = count++;
+}, false);
