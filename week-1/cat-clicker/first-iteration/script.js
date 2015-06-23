8c8d427a13b0b1a2e8bf83f7780a920f58c49570
@shmulim <shmuel.markel@gmail.com>
2015-06-23 00:32:23 -0700

    Update script.js

--- week-1/cat-clicker/first-iteration/script.js
+++ week-1/cat-clicker/first-iteration/script.js
@@ -1,7 +1,12 @@
-var cat = document.getElementById('cat');
-var counter = document.getElementById('counter');
-var count = 0;
+(function(){
+    'use strict';
+    
+    var cat = document.getElementById('cat');
+    var counter = document.getElementById('counter');
+    var count = 0;
 
-cat.addEventListener('click', function () {
-    counter.innerHTML = count++;
-}, false);
+    cat.addEventListener('click', function () {
+        counter.innerHTML = count++;
+    }, false);
+
+})();
